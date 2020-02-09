ce36156d8c0cde69ec778edd9c3ef8796eb99c0f
AlexP <alex.p.panteleev@gmail.com>
2020-02-09 21:50:21 +0300

    add Helper\Html

---
+++ src/Helper/Html.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace BxHelper\Helper;
+
+
+use BxHelper\Html\ {
+    A,
+    Input
+};
+
+
+/**
+ * Class Html
+ * @package BxHelper\Helper
+ *
+ * @author AlexP007
+ * @email alex.p.panteleev@gmail.com
+ * @link https://github.com/AlexP007/bx-helper
+ */
+class Html
+{
+    private static function setParams(array $params)
+    {
+        $attributes = $params['attr'] ?? [];
+        $options = $params['options'] ?? [];
+
+        return [$attributes, $options];
+    }
+
+    public static function a(string $content, string $href, array $params = [])
+    {
+        [$attributes, $options] = self::setParams($params);
+
+        $attributes['href'] = $href;
+
+        $elt = new A($attributes, $options);
+        $elt->setContent($content);
+
+        return $elt->render();
+    }
+
+    public static function input(string $type, string $value = null, array $params = [])
+    {
+        [$attributes, $options] = self::setParams($params);
+
+        $attributes['type'] = $type;
+
+        if ($value) {
+            $attributes['value'] = $value;
+        }
+
+        $elt = new Input($attributes, $options);
+
+        return $elt->render();
+    }
+}
